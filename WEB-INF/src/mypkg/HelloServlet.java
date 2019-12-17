b46b17979e0bdd9d6aeddf8674492ccbcbc4af91
ecwpz91 <ecwpz91@users.noreply.github.com>
2019-12-17 10:24:14 -0500

    Added environment variable check and display

--- WEB-INF/src/mypkg/HelloServlet.java
+++ WEB-INF/src/mypkg/HelloServlet.java
@@ -1,42 +1,49 @@
 package mypkg;
- 
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
- 
+
 public class HelloServlet extends HttpServlet {
-   @Override
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
-               throws IOException, ServletException {
-      
-      // Set the response message's MIME type
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-      response.setContentType("text/html;charset=UTF-8");
+		// Set the response message's MIME type
 
-      // Allocate a output writer to write the response message into the network socket
+		response.setContentType("text/html;charset=UTF-8");
 
-      PrintWriter out = response.getWriter();
-      
-      // Write the response message, in an HTML page
+		// Allocate a output writer to write the response message into the network
+		// socket
 
-      try {
-        out.println("<!DOCTYPE html>");
-        out.println("<html><head>");
-        out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
-        out.println("<title>Hello, World</title></head>");
-        out.println("<body>");
-        out.println("<h1>Hello!</h1>");  // says Hello
-        // Echo client's request information
-        out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
-        out.println("<p>Protocol: " + request.getProtocol() + "</p>");
-        out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
-        out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
-        // Generate a random number upon each request
-        out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
-        out.println("</body>");
-        out.println("</html>");
-      } finally {
-        out.close();  // Always close the output writer
-      }
-   }
+		PrintWriter out = response.getWriter();
+
+		// Write the response message, in an HTML page
+
+		try {
+			String value = System.getenv("ENVAR");
+
+			out.println("<!DOCTYPE html>");
+			out.println("<html><head>");
+			out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+			out.println("<title>Hello, World</title></head>");
+			out.println("<body>");
+			out.println("<h1>HelloServlet!</h1>"); // Says HelloServlet
+			// Echo client's request information
+			out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+			out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+			out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+			out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+			// Generate a random number upon each request
+			out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+
+			if (value != null && !value.isEmpty()) {
+				out.println("<p>Environment variable:" + value + "</p>");
+			}
+
+			out.println("</body>");
+			out.println("</html>");
+		} finally {
+			out.close(); // Always close the output writer
+		}
+	}
 }
